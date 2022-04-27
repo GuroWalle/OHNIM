f655a0a27d6eaa45aaaf35f5fe129fd80efa5b5e
Guro Walle <guro.terjesdatter.walle@gmail.com>
2022-04-27 10:16:49 +0200

    Made sanity

---
+++ app/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>OHNIM / MINO</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appName: import.meta.env.VITE_APP_NAME,
+    };
+  },
+};
+</script>
+
