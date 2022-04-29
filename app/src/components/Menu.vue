a07cd4b7d299f4a1120651fd588793817bd19678
Guro Walle <guro.terjesdatter.walle@gmail.com>
2022-04-29 16:13:29 +0200

    Toggle menu is working.

--- app/src/components/Menu.vue
+++ app/src/components/Menu.vue
@@ -1,6 +1,103 @@
 <template>
-  <h1>Menu</h1>
-  <RouterLink to="/">
-    <div class="main-page__logo">MINO / OHNIM</div>
-  </RouterLink>
+  <div class="menu">
+    <button @click="openMenu">
+      <img
+        class="menu__hamburger"
+        v-if="hamburger_showing"
+        src="/assets/images/hamburger.svg"
+        alt=""
+      />
+    </button>
+    <div v-if="x_showing" class="menu__links">
+      <button class="menu__links--x" @click="closeMenu" to="/">X</button>
+      <RouterLink to="/works">Works</RouterLink> <br />
+      <RouterLink to="/biography">Biography</RouterLink> <br />
+      <RouterLink to="/exhibitions">Exhibitions</RouterLink> <br />
+      <RouterLink to="/contact">Contact</RouterLink>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      x_showing: false,
+      hamburger_showing: true,
+    };
+  },
+  methods: {
+    openMenu() {
+      this.x_showing = true;
+      this.hamburger_showing = false;
+    },
+    closeMenu() {
+      this.x_showing = false;
+      this.hamburger_showing = true;
+    },
+  },
+};
+</script>
+
+<style>
+.menu {
+  position: relative;
+  width: 15.938rem;
+  height: 49.3rem;
+  background: var(--white-transparent);
+  left: 0;
+  top: 0;
+}
+
+.menu__hamburger {
+  position: absolute;
+  width: 3.5rem;
+  top: 3.8rem;
+  left: 6.75rem;
+}
+
+.menu__links--x {
+  display: flex;
+  justify-content: center;
+  padding: var(--sizing-medium);
+  font-size: var(--font-big-desktop);
+  color: black;
+  text-decoration: none;
+}
+
+.menu__links a {
+  display: flex;
+  flex-direction: column;
+  margin-left: var(--sizing-small);
+  padding: var(--sizing-small);
+
+  font-size: var(--font-small-desktop);
+  color: black;
+  letter-spacing: var(--letter-spazing-small);
+  text-decoration: none;
+}
+
+.menu__links a:hover {
+  margin-left: var(--sizing-medium);
+}
+
+/*   mobile   */
+@media screen and (max-width: 600px) {
+  .menu {
+    position: relative;
+    width: 9.875rem;
+    height: 49.3rem;
+    background: pink;
+    left: 0;
+    top: 0;
+  }
+
+  .menu__links--x {
+    font-size: var(--font-big-mobile);
+  }
+
+  .menu__links a:nth-child(n + 2) {
+    font-size: var(--font-small-mobile);
+  }
+}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,21 +2,21 @@
   <div class="main-page">
     <div class="main-page__logo">MINO / OHNIM</div>
     <div class="main-page__background">
-      <div class="main-page__menu">
-        <RouterLink to="/works">Works</RouterLink> <br />
-        <RouterLink to="/biography">Biography</RouterLink> <br />
-        <RouterLink to="/exhibitions">Exhibitions</RouterLink> <br />
-        <RouterLink to="/contact">Contact</RouterLink>
-      </div>
+      <Menu />
     </div>
     <!-- <img src="/assets/images/background_desktop.jpg" alt="" /> -->
   </div>
 </template>
 
 <script>
+import Menu from "../components/Menu.vue";
 export default {
   data() {
     return {};
+  },
+
+  components: {
+    Menu,
   },
 };
 </script>
@@ -24,10 +24,11 @@ export default {
 <style>
 .main-page__logo {
   position: absolute;
+  top: 3.8rem;
   right: 6.75rem;
   color: red;
-  font-size: 3.125rem;
-  letter-spacing: 0.313rem;
+  font-size: var(--font-big-desktop);
+  letter-spacing: var(--letter-spazing-medium);
 }
 
 .main-page__background {
@@ -36,17 +37,13 @@ export default {
   height: 100%;
 }
 
-.main-page__menu {
-  color: white;
-  font-size: 25px;
-}
-
 /*   mobile   */
 @media screen and (max-width: 600px) {
   .main-page__logo {
-    right: 3.563rem;
+    right: 2.563rem;
     color: blue;
-    font-size: 1.875rem;
+    font-size: var(--font-big-mobile);
+    width: 9.9rem;
   }
 }
 </style>
